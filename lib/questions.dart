bdb81344a2137161a16223c50567c9d6a4d327de
Chadthida <645051200014@mail.rmutk.ac.th>
2024-02-09 18:46:46 +0700

    Chadthida

---
+++ lib/questions.dart
@@ -0,0 +1,114 @@
+import 'question.dart';
+
+const List<Question> questions = [
+  //คำถาม 1
+  Question(
+    correctAnswerIndex: 0,
+    question: 'เกม ROV เปิดตัวครั้งแรกปีใด?',
+    option: [
+      'a) 2015 ',
+      'b) 2016',
+      'c) 2017',
+      'd) 2018'
+    ],
+  ),
+  //คำถาม 2
+  Question(
+    correctAnswerIndex: 2,
+    question: 'เกม ROV มีกี่ตำแหน่ง?',
+    option: [
+      'a) 3',
+      'b) 4',
+      'c) 5',
+      'd) 6'
+    ],
+  ),
+  //คำถาม 3
+  Question(
+    correctAnswerIndex: 2,
+    question: 'Arena of Valor มีชื่อเก่าว่าอะไร?',
+    option: [
+      'a) ลีกออฟเลเจ็นดส์(LOL)',
+      'b) ฮีโร่ออฟนิวเวิส(HON)',
+      'c) เรียล์มออฟเวเลอร์(ROV)',
+      'd) อารีนาออฟเวเลอร์(AOV)'
+    ],
+  ),
+  //คำถาม 4
+  Question(
+    correctAnswerIndex: 1,
+    question: 'ฮีโร่ล่าสุดในเกม ROV? (9/2/67)',
+    option: [
+      'a) Bijan',
+      'b) Erin',
+      'c) Ming',
+      'd) Bonnie'
+    ],
+  ),
+  //คำถาม 5
+  Question(
+    correctAnswerIndex: 3,
+    question: 'ฮีไร่ใดต่างจากพวก?',
+    option: [
+      'a) Violet',
+      'b) Teeri',
+      'c) Erin',
+      'd) Iggy'
+    ],
+  ),
+  //คำถาม 6
+  Question(
+    correctAnswerIndex: 3,
+    question: 'Helen ถูกนำมาแทนฮีโร่ใด?',
+    option: [
+      'a) Preyta',
+      'b) Batman',
+      'c) Yue',
+      'd) Payna'
+    ],
+  ),
+  //คำถาม 7
+  Question(
+    correctAnswerIndex: 0,
+    question: 'แรงค์สูงสุดในเกม ROV?',
+    option: [
+      'a) Glorious Ruler',
+      'b) Supreme Conqueror',
+      'c) Conqueror',
+      'd) Commander'
+    ],
+  ),
+  //คำถาม 8
+  Question(
+    correctAnswerIndex: 1,
+    question: 'ฮีโร่ทั้งหมดในเกม ROV มีกี่ฮีโร่? (9/2/67)',
+    option: [
+      'a) 118 ฮีโร่',
+      'b) 117 ฮีโร่',
+      'c) 116 ฮีโร่',
+      'd) 115 ฮีโร่'
+    ],
+  ),
+  //คำถาม 9
+  Question(
+    correctAnswerIndex: 0,
+    question: 'สกินทั้งหมดในเกม ROV มีกี่สกิน? (9/2/67)',
+    option: [
+      'a) 804 สกิน',
+      'b) 803 สกิน',
+      'c) 802 สกิน',
+      'd) 801 สกิน'
+    ],
+  ),
+  //คำถาม 10
+  Question(
+    correctAnswerIndex: 0,
+    question: 'ฮีไร่ใดเป็นฮีโร่ชาย?',
+    option: [
+      'a) Iggy',
+      'b) Bonnie',
+      'c) Veera',
+      'd) Krixi'
+    ],
+  ),
+];
